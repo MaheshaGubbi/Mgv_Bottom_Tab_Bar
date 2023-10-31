d28e5bc60471d5ea684d98634a0dadaf8bfbc6d5
MaheshGubbi <maheshguubi44@gmail.com>
2023-10-31 14:43:56 +0530

    Initial Commit

--- app/src/main/java/com/mgvbottomtabbar/presentation/Screens.kt
+++ app/src/main/java/com/mgvbottomtabbar/presentation/Screens.kt
@@ -6,23 +6,16 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.mgvbottomtabbar.AutoComplete
 
 @Composable
 fun MessageScreen() {
     Column {
-
-        Text(text = "Auto suggest and Dropdown field",
-            fontSize = 18.sp,
-                    modifier = Modifier.padding(30.dp))
-        AutoComplete()
+        CenterText(text = "Message")
     }
 }
 @Composable
